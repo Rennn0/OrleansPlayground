0507dcfd45a7e3a12b338b83f457fa487f6f5339
Rennn0 <leqterhanibal@gmail.com>
2025-09-30 01:34:56 +0400

    h

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,8 +1,19 @@
 using Azure.Data.Tables;
+using Microsoft.OpenApi.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Orleans API",
+        Version = "v1",
+        Description = "API for Orleans client"
+    });
+});
 
 builder.UseOrleansClient(client =>
 {
@@ -11,10 +22,17 @@ builder.UseOrleansClient(client =>
         opt.TableServiceClient =
             new TableServiceClient(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION") ??
                                    throw new Exception());
-    }).UseConnectionRetryFilter((exception, token) => Task.FromResult(true));
+    }).UseConnectionRetryFilter(async (exception, token) =>
+    {
+        await Task.Delay(TimeSpan.FromSeconds(10), token);
+        return true;
+    });
 });
 
 WebApplication app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Orleans API v1"); });
 
 app.UseHttpsRedirection();
 
